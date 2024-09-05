ff30dc367e7452f612a29b3992efcc0ce953cf6b
Kang-min Liu <gugod@gugod.org>
2024-09-05 16:36:48 +0900

    let the width of main/side be configurable.

--- init.lua
+++ init.lua
@@ -8,57 +8,79 @@ obj.author = "Kang-min Liu <gugod@gugod.org>"
 obj.homepage = "https://github.com/gugod/TwoStackWM.spoon"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
+obj.config = {}
+obj.config.mainWidth = 13
+obj.config.sideWidth = 8
+
 function obj:init()
    hs.window.animationDuration = 0
    hs.grid.setMargins(hs.geometry.point(4, 4))
-   hs.grid.setGrid('21x21')
+   hs.grid.setGrid((self.config.mainWidth + self.config.sideWidth) .. "x1")
 end
 
 function obj:moveFocusedWindowToMainStack()
-  local win = hs.window.focusedWindow()
-  local cell = hs.grid.get(win)
-  cell.x = 8
-  cell.y = 0
-  cell.h = 21
-  cell.w = 13
-  hs.grid.set(win, cell)
+   local win = hs.window.focusedWindow()
+   local cell = hs.grid.get(win)
+   cell.x = self.config.sideWidth
+   cell.y = 0
+   cell.h = 1
+   cell.w = self.config.mainWidth
+   hs.grid.set(win, cell)
 end
 
 function obj:moveFocusedWindowToSideStack()
-  local win = hs.window.focusedWindow()
-  local cell = hs.grid.get(win)
-  cell.x = 0
-  cell.y = 0
-  cell.h = 21
-  cell.w = 8
-  hs.grid.set(win, cell)
+   local win = hs.window.focusedWindow()
+   local cell = hs.grid.get(win)
+   cell.x = 0
+   cell.y = 0
+   cell.h = 1
+   cell.w = self.config.sideWidth
+   hs.grid.set(win, cell)
 end
 
 function obj:toggleFocusedWindowToMainOrSide()
-  local win = hs.window.focusedWindow()
-  local cell = hs.grid.get(win)
-  if cell.x < 8
-  then
-    self:moveFocusedWindowToMainStack()
-  else
-    self:moveFocusedWindowToSideStack()
-  end
+   local win = hs.window.focusedWindow()
+   local cell = hs.grid.get(win)
+   if cell.x < 8
+   then
+      self:moveFocusedWindowToMainStack()
+   else
+      self:moveFocusedWindowToSideStack()
+   end
+end
+
+function obj:toggleFocusedWindowToMaxOrMainOrSide()
+   -- side -> main -> max -> side ...
+   local win = hs.window.focusedWindow()
+   local cell = hs.grid.get(win)
+
+   if cell.w == self.config.sideWidth then
+      -- side -> main
+      self:moveFocusedWindowToMainStack()
+
+   elseif cell.w == self.config.mainWidth then
+      -- main -> max
+      self:maximizeFocusedWindow()
+   else
+      -- max -> side
+      self:moveFocusedWindowToSideStack()
+   end
 end
 
 function obj:maximizeFocusedWindow()
-  local win = hs.window.focusedWindow()
-  local cell = hs.grid.get(win)
-  cell.x = 0
-  cell.y = 0
-  cell.h = 21
-  cell.w = 21
-  hs.grid.set(win, cell)
+   local win = hs.window.focusedWindow()
+   local cell = hs.grid.get(win)
+   cell.x = 0
+   cell.y = 0
+   cell.h = 1
+   cell.w = self.config.mainWidth + self.config.sideWidth
+   hs.grid.set(win, cell)
 end
 
 function obj:moveFocusedWindowToNextScreen()
-  local win = hs.window.focusedWindow()
-  local screen = win:screen()
-  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+   local win = hs.window.focusedWindow()
+   local screen = win:screen()
+   win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
 end
 
 return obj
